237102222619d6c60596ff160660bded9b74ac2d
Nicolay <nicolaymorenoherrera@gmail.com>
2024-03-21 15:25:31 -0500

    Clases-Objetos-Constructores-Herencia

--- 2_Clases_Objetos_Constructores/Program.cs
+++ 2_Clases_Objetos_Constructores/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _2_Clases_Objetos_Constructores.Models;
+
+
+// Creando instancia de una clase:
+var bebida = new Bebida( "Coca cola", 1000 );
+bebida.Beberse(500);
+Console.WriteLine(bebida.Cantidad);
+
+// Usando la clase Cerveza que hereda de Bebida:
+//var cerveza = new Cerveza("Aguila", 800);
+var cerveza = new Cerveza(1500, "Poker");
+
+cerveza.Beberse(250);
+Console.WriteLine(cerveza.Cantidad);
