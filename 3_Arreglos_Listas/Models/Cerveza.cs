8313f394dc1280dd442000d3cf3d3e918e683113
Nicolay <nicolaymorenoherrera@gmail.com>
2024-03-21 17:56:34 -0500

    Arreglos y listas

---
+++ 3_Arreglos_Listas/Models/Cerveza.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace _2_Clases_Objetos_Constructores.Models
+{
+    internal class Cerveza : Bebida
+    {
+        // Puedo definir parametros por defecto desde el inicio cuando heredo.
+        //Constructor:
+        //public Cerveza() : base("cerveza", 1000) { }
+
+        // Tambien puedo definir el contructor de esta clase que hereda:
+        //public Cerveza(string Nombre, int Cantidad) : base(Nombre, Cantidad) { }
+
+        // Podemos poner parametros por defecto o opcional, pero los que vienen por defecto tienen que ir de ultimas:
+        public Cerveza(int Cantidad, string Nombre = "Pilsen")
+            : base(Nombre, Cantidad)
+        {
+
+        }
+
+    }
+}
---
+++ 3_Arreglos_Listas/Program.cs
@@ -0,0 +1,51 @@
+﻿using _2_Clases_Objetos_Constructores.Models;
+
+// Cola (queue) => el primero en la fila es el primero en salir.
+// Pila (stack) => el último en la fila es el primero en salir.
+
+// Array
+
+//int[] numeros = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Definiando longitud del array desde el comienzo.
+
+int[] numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // Definiando longitud del array desde el comienzo.
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Iteración {i} - {numeros[i]}");
+}
+
+Console.WriteLine("\n");
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine($"Iteración {numero} - {numeros[numero]}");
+}
+
+Console.WriteLine("\n************************************************************************");
+// Las lista es una clase de C# que implementan regularmente una interfaz. Los arreglos siempre van a ser mas rapido que una lista ya que esta orientado a una coleccion de objetos. 
+
+List<int> lista = new List<int>();
+
+List<int> lista2 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+lista2.Add(9);
+lista2.Add(0);
+lista2.Remove(2);
+
+foreach (var numero in lista2)
+{
+    Console.WriteLine(numero);
+}
+
+Console.WriteLine("\n************************************************************************");
+
+// Tambien puedo crear una lista de una clase.
+// Formas de crear el objeto para agregarlo a lista clase Cerveza:
+List<Cerveza> cervezas = new List<Cerveza>() { new Cerveza(10, "Premium") };
+cervezas.Add(new Cerveza(500));
+Cerveza erdinger = new Cerveza(400, "Trigo");
+cervezas.Add(erdinger);
+
+foreach (var cerveza in cervezas)
+{
+    Console.WriteLine($" Cerveza: {cerveza.Nombre} | Cantidad: {cerveza.Cantidad}");
+}
