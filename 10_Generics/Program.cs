a4d7cfa0b22b8b0e3da7cb60a287f84e9eaf59af
Nicolay <nicolaymorenoherrera@gmail.com>
2024-04-01 19:53:06 -0500

    Generics

---
+++ 10_Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using _10_Generics.Service;
+using _2_Clases_Objetos_Constructores.Models;
+using _8_Solicitudes_serviciosWeb_por_HTTP_Get.Models;
+
+Cerveza cerveza = new Cerveza(500, "Ticús")
+{ Alcohol = 5, Marca = "Colima" };
+
+SendRequest<Cerveza> service = new SendRequest<Cerveza>();
+Cerveza cervezaRespuesta = await service.Send(cerveza);
+
+Console.WriteLine("ok");
+
+
+// Como estoy trabajando con generics, establezco desde el comienzo con que tipo de objeto voy a trabajar:
+
+Post post = new Post { body = "Soy un body", title = "hola", userId = 50 };
+
+SendRequest<Post> service2 = new SendRequest<Post>();
+Post postRespuesta = await service2.Send(post);
+
+Console.WriteLine("ok");
---
+++ 10_Generics/Service/SendRequest.cs
@@ -0,0 +1,33 @@
+﻿
+
+using _10_Generics.Models;
+using System.Text.Json;
+
+namespace _10_Generics.Service
+{
+    // La T indica que va a recibir una clase que la va a definir el objeto con el que va a trabajar.
+    public class SendRequest<T> where T : IRequestable
+    {
+        private HttpClient _client = new HttpClient();
+
+        public async Task<T> Send(T model)
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts/";
+
+            string data = JsonSerializer.Serialize<T>(model);
+            HttpContent content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage httpResponse = await _client.PostAsync(url, content);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                string result = await httpResponse.Content.ReadAsStringAsync();
+
+                var postResult = JsonSerializer.Deserialize<T>(result);
+
+                return postResult;
+            }
+
+            return default(T);
+        }
+    }
+}
