d389b648ff71e23d7c059b999739a129e390266d
Nicolay <nicolaymorenoherrera@gmail.com>
2024-04-04 21:19:57 -0500

    Predicate

---
+++ 15_Predicate/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+var numbers = new List<int> { 1, 56, 2, 3, 3, 45, 6 };
+
+//var predicate = new Predicate<int>(IsDivider2);
+//bool IsDivider2(int x) => x % 2 == 0;
+
+// Estableciendo predicate con una funcion anonima.
+var predicate = new Predicate<int>(x => x % 2 == 0);
+var dividers2 = numbers.FindAll(predicate);
+
+dividers2.ForEach(d => { Console.WriteLine(d); });
+
+// Otro ejemplo.
+Console.WriteLine();
+Predicate<int> negativepRedicate = new Predicate<int>(x => !predicate(x));
+dividers2 = numbers.FindAll(negativepRedicate);
+dividers2.ForEach(d => { Console.WriteLine(d); });
+
+// Otro ejemplo, esta vez con cervezas.
